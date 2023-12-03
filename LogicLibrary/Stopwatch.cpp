8c0f5a23e0a8c7186f02d038f10cbf4cf5bfc6c0
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-03 12:10:56 +0100

    implementation of stopwatch

--- LogicLibrary/Stopwatch.cpp
+++ LogicLibrary/Stopwatch.cpp
@@ -1,1 +1,34 @@
 #include "Stopwatch.h"
+
+Stopwatch::Stopwatch() : running(false) {}
+
+void Stopwatch::start() {
+    if (!running) {
+        running = true;
+        startTime = Clock::now();
+        previousTime = startTime;
+    }
+}
+
+void Stopwatch::stop() {
+    if (running) {
+        previousTime = Clock::now();
+        running = false;
+    }
+}
+
+void Stopwatch::reset() {
+    startTime = Clock::now();
+    previousTime = startTime;
+}
+
+double Stopwatch::getDetalTime() {
+    if (running) {
+        auto currentTime = Clock::now();
+        auto deltaTime = std::chrono::duration_cast<std::chrono::duration<double>>(currentTime - previousTime);
+        previousTime = currentTime;
+        return deltaTime.count();
+    } else {
+        return 0.0;
+    }
+}
--- LogicLibrary/Stopwatch.h
+++ LogicLibrary/Stopwatch.h
@@ -1,8 +1,25 @@
 #ifndef AP_PROJECT_PAC_MAN_STOPWATCH_H
 #define AP_PROJECT_PAC_MAN_STOPWATCH_H
 
+#include <chrono>
 
 class Stopwatch {
+    Stopwatch();
+    ~Stopwatch() = default;
+
+    void start();
+    void stop();
+    void reset();
+
+    double getDetalTime();
+
+private:
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+
+    TimePoint startTime;
+    TimePoint previousTime;
+    bool running;
 
 };
 
