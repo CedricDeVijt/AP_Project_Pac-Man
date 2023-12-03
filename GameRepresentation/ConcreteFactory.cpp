d5b7402a308c14af3e83697185f924eb4599b596
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-03 10:38:28 +0100

    basic implementation of ConcreteFactory

--- GameRepresentation/ConcreteFactory.cpp
+++ GameRepresentation/ConcreteFactory.cpp
@@ -1,1 +1,21 @@
 #include "ConcreteFactory.h"
+
+Ghost* ConcreteFactory::createGhost() {
+    return new Ghost();
+}
+
+PacMan* ConcreteFactory::createPacMan() {
+    return new PacMan();
+}
+
+Wall* ConcreteFactory::createWall(const WallType wallType) {
+    return new Wall(wallType);
+}
+
+Coin* ConcreteFactory::createCoin() {
+    return new Coin();
+}
+
+Fruit* ConcreteFactory::createFruit() {
+    return new Fruit();
+}
--- GameRepresentation/ConcreteFactory.h
+++ GameRepresentation/ConcreteFactory.h
@@ -1,8 +1,15 @@
 #ifndef AP_PROJECT_PAC_MAN_CONCRETEFACTORY_H
 #define AP_PROJECT_PAC_MAN_CONCRETEFACTORY_H
 
+#include "../LogicLibrary/AbstractFactory.h"
 
-class ConcreteFactory {
+class ConcreteFactory final : public AbstractFactory{
+
+    Ghost* createGhost();
+    PacMan* createPacMan();
+    Wall* createWall(const WallType wallType);
+    Coin* createCoin();
+    Fruit* createFruit();
 
 };
 
