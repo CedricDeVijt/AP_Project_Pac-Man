54291699608ff96a969742450c6d41575232b36c
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-03 10:40:01 +0100

    basic implementation of State

--- GameRepresentation/State.h
+++ GameRepresentation/State.h
@@ -1,25 +1,61 @@
 #ifndef AP_PROJECT_PAC_MAN_STATE_H
 #define AP_PROJECT_PAC_MAN_STATE_H
 
+#include "StateManager.h"
 
 class State {
+protected:
+    StateManager *stateManager;
+    void createNewLevelState();
+    void createNewMenuState();
 
+
+public:
+    explicit State(StateManager *stateManager);
+    virtual ~State() = default;
 };
 
-class MenuState : public State {
+class MenuState final : public State {
+public:
+    explicit MenuState(StateManager *stateManager);
 
+private:
+    void toLevelState();
 };
 
-class LevelState : public State {
+class LevelState final : public State {
+public:
+    explicit LevelState(StateManager *stateManager);
 
+private:
+    void toVictoryState();
+    void toPausedState();
+    void toGameOverState();
 };
 
-class PauseState : public State {
+class PausedState final : public State {
+public:
+    explicit PausedState(StateManager *stateManager);
 
+private:
+    void toMenuState();
+    void toLevelState();
 };
 
-class VictoryState : public State {
+class VictoryState final : public State {
+public:
+    explicit VictoryState(StateManager *stateManager);
 
+private:
+    void toLevelState();
+};
+
+class GameOverState final : public State {
+public:
+    explicit GameOverState(StateManager *stateManager);
+
+private:
+    void toMenuState();
 };
 
 #endif //AP_PROJECT_PAC_MAN_STATE_H
