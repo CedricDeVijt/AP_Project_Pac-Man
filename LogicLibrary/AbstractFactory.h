1058978da5e51747d590cd18e8d36c843c6e031c
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-03 10:37:29 +0100

    implementation of abstract factory

--- LogicLibrary/AbstractFactory.h
+++ LogicLibrary/AbstractFactory.h
@@ -1,8 +1,21 @@
 #ifndef AP_PROJECT_PAC_MAN_ABSTRACTFACTORY_H
 #define AP_PROJECT_PAC_MAN_ABSTRACTFACTORY_H
-#include "../GameRepresentation/ConcreteFactory.h"
 
-class AbstractFactory : public ConcreteFactory{
+#include "Ghost.h"
+#include "PacMan.h"
+#include "Wall.h"
+#include "Collectable.h"
+
+class AbstractFactory {
+public:
+
+    virtual ~AbstractFactory() = default;
+
+    virtual Ghost* createGhost() = 0;
+    virtual PacMan* createPacMan() = 0;
+    virtual Wall* createWall(const WallType wallType) = 0;
+    virtual Coin* createCoin() = 0;
+    virtual Fruit* createFruit() = 0;
 
 };
 
