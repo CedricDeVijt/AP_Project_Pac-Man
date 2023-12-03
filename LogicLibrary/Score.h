e379a9df6dae1f7f8289f8707dfa6d46ec204c0a
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-03 10:39:50 +0100

    basic implementation of score

--- LogicLibrary/Score.h
+++ LogicLibrary/Score.h
@@ -1,11 +1,12 @@
 #ifndef AP_PROJECT_PAC_MAN_SCORE_H
 #define AP_PROJECT_PAC_MAN_SCORE_H
 
-
+#include <vector>
 #include "Observer.h"
 
 class Score : public Observer {
-
+    std::vector<std::pair<std::string, int>> highScores;
+    int currentScore;
 };
 
 
