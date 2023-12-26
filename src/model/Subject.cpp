c00de27f40f77969ba110d911a1c5aa5afe47aa4
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-26 21:48:49 +0100

    Basic implementation

--- src/model/Subject.cpp
+++ src/model/Subject.cpp
@@ -1,1 +1,11 @@
 #include "Subject.h"
+
+void Subject::registerObserver(shared_ptr<Observer> observer) {
+    observers.push_back(observer);
+}
+
+void Subject::notifyObservers() {
+    for (auto &observer : observers) {
+        observer->update();
+    }
+}
--- src/model/Subject.h
+++ src/model/Subject.h
@@ -1,9 +1,22 @@
 #ifndef AP_PROJECT_PAC_MAN_SUBJECT_H
 #define AP_PROJECT_PAC_MAN_SUBJECT_H
 
+#include <vector>
+#include <memory>
+
+#include "Observer.h"
+
+using std::shared_ptr;
+
 
 class Subject {
+public:
+    void registerObserver(shared_ptr<Observer> observer);
 
+private:
+    std::vector<shared_ptr<Observer>> observers;
+
+    void notifyObservers();
 };
 
 
