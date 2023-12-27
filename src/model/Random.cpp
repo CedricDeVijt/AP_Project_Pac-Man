e1a019856c87f29d0d5cf812ec1e48accfda691d
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-27 20:46:20 +0100

    basic implementation of Random

--- src/model/Random.cpp
+++ src/model/Random.cpp
@@ -1,1 +1,18 @@
 #include "Random.h"
+#include <iostream>
+
+// Define the static instance of the singleton
+Random::Random() {
+    // Constructor code, if needed
+}
+
+Random& Random::getInstance() {
+    static Random instance; // Guaranteed to be destroyed, instantiated on first use
+    return instance;
+}
+
+int Random::getRandomNumber() {
+    // Generate a random number between 1 and 100
+    std::uniform_int_distribution<int> distribution(1, 100);
+    return distribution(generator);
+}
--- src/model/Random.h
+++ src/model/Random.h
@@ -1,10 +1,27 @@
 #ifndef AP_PROJECT_PAC_MAN_RANDOM_H
 #define AP_PROJECT_PAC_MAN_RANDOM_H
 
-
+#include <random>
 class Random {
+private:
+    // Private constructor to prevent external instantiation
+    Random();
 
+public:
+    // Static method to get the instance of the singleton
+    static Random& getInstance();
+
+    // Member function of the singleton
+    int getRandomNumber();
+
+    // Prevent copying and assignment
+    Random(const Random&) = delete;
+    Random& operator=(const Random&) = delete;
+
+private:
+    // Random number generator
+    // TODO mersenne
+    std::default_random_engine generator;
 };
-
 
 #endif //AP_PROJECT_PAC_MAN_RANDOM_H
