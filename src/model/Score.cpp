0d88b9d1fbf4ae00cb28e213f8963d51fc5527fa
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-27 21:41:16 +0100

    added save and load score

--- src/model/Score.cpp
+++ src/model/Score.cpp
@@ -1,1 +1,63 @@
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 #include "Score.h"
+
+void Score::saveHighScore(std::string name, int score, std::string fileName) {
+    auto scores = getHighScores(fileName);
+    scores.emplace_back(name, score);
+
+    // sort the scores
+    std::sort(scores.begin(), scores.end(), [](const auto&a, const auto&b) {
+        return a.second > b.second; // Sort in descending order based on scores
+    });
+
+    // discard the lowest score
+    scores.pop_back();
+
+    // write the scores to the file
+    std::ofstream file(fileName, std::ios::trunc);
+    if (!file.is_open()) {
+        throw std::runtime_error("Error opening high scores file");
+    } else {
+        file << "Name, Score\n";
+        for (const auto& scorePair : scores) {
+            file << scorePair.first << ", " << scorePair.second << "\n";
+        }
+        file.close();
+    }
+}
+
+std::vector<std::pair<std::string, int>> Score::getHighScores(const std::string&fileName) {
+    std::ifstream file(fileName);
+    std::vector<std::pair<std::string, int>> highScores;
+
+    if (file.is_open()) {
+        std::string line;
+        // Skip the header line
+        std::getline(file, line);
+
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::string name;
+            int score;
+
+            if (iss >> name >> score) {
+                highScores.emplace_back(name, score);
+            }
+            else {
+                // Handle parsing error if needed
+                throw std::runtime_error("Error parsing high scores file");
+            }
+        }
+
+        file.close();
+    }
+    else {
+        // Handle file opening error if needed
+        throw std::runtime_error("Error opening high scores file");
+    }
+
+    return highScores;
+}
--- src/model/Score.h
+++ src/model/Score.h
@@ -6,8 +6,13 @@
 #include "Observer.h"
 
 class Score : public Observer {
-    std::vector<std::pair<std::string, int>> highScores;
+    // std::vector<std::pair<std::string, int>> highScores;
     int currentScore;
+
+public:
+    void saveHighScore(std::string name, int score, std::string fileName);
+
+    std::vector<std::pair<std::string, int>> getHighScores(const std::string &fileName);
 };
 
 
