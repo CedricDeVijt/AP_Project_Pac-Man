a3f3280513b9313f28e56c02dd3271c2cf147ccb
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-27 22:28:52 +0100

    small bug fixes

--- src/model/Score.cpp
+++ src/model/Score.cpp
@@ -7,32 +7,36 @@
 
 #include "Score.h"
 
-void Score::saveHighScore(std::string name, int score, std::string fileName) {
-    auto scores = getHighScores(fileName);
+void Score::saveScore(std::string name, int score) {
+    auto scores = loadHighScores();
     scores.emplace_back(name, score);
 
     // sort the scores
-    std::sort(scores.begin(), scores.end(), [](const auto&a, const auto&b) {
+    std::sort(scores.begin(), scores.end(), [](const auto &a, const auto &b) {
         return a.second > b.second; // Sort in descending order based on scores
     });
 
     // discard the lowest score
-    scores.pop_back();
+    if (scores.size() >= 5) {
+        scores.pop_back();
+    }
 
     // write the scores to the file
+    const std::string fileName = "data/high_scores.csv";
     std::ofstream file(fileName, std::ios::trunc);
     if (!file.is_open()) {
         throw std::runtime_error("Error opening high scores file");
     } else {
         file << "Name, Score\n";
-        for (const auto& scorePair : scores) {
+        for (const auto &scorePair: scores) {
             file << scorePair.first << ", " << scorePair.second << "\n";
         }
         file.close();
     }
 }
 
-std::vector<std::pair<std::string, int>> Score::getHighScores(const std::string&fileName) {
+std::vector<std::pair<std::string, int>> Score::loadHighScores() {
+    const std::string fileName = "data/high_scores.csv";
     std::ifstream file(fileName);
     std::vector<std::pair<std::string, int>> highScores;
 
@@ -44,20 +48,21 @@ std::vector<std::pair<std::string, int>> Score::getHighScores(const std::string&
         while (std::getline(file, line)) {
             std::istringstream iss(line);
             std::string name;
-            int score;
+            std::string score;
 
-            if (iss >> name >> score) {
-                highScores.emplace_back(name, score);
-            }
-            else {
-                // Handle parsing error if needed
-                throw std::runtime_error("Error parsing high scores file");
-            }
+            std::getline(iss, name, ',');
+            std::getline(iss, score, ',');
+            highScores.emplace_back(name, std::stoi(score));
+//
+//            if (iss >> name >> score) {
+//            } else {
+//                // Handle parsing error if needed
+//                throw std::runtime_error("Error parsing high scores file");
+//            }
         }
 
         file.close();
-    }
-    else {
+    } else {
         // Handle file opening error if needed
         throw std::runtime_error("Error opening high scores file");
     }
--- src/model/Score.h
+++ src/model/Score.h
@@ -6,13 +6,12 @@
 #include "Observer.h"
 
 class Score : public Observer {
-    // std::vector<std::pair<std::string, int>> highScores;
     int currentScore;
 
 public:
-    void saveHighScore(std::string name, int score, std::string fileName);
+    static void saveScore(std::string name, int score);
 
-    std::vector<std::pair<std::string, int>> getHighScores(const std::string &fileName);
+    static std::vector<std::pair<std::string, int>> loadHighScores();
 };
 
 
