e0d0cf4a80dc09a454bb997b3c8dcc94fdb44e4b
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-28 15:26:01 +0100

    Use of shared pointers

--- src/model/AbstractFactory.h
+++ src/model/AbstractFactory.h
@@ -5,17 +5,21 @@
 #include "PacMan.h"
 #include "Wall.h"
 #include "Collectable.h"
+#include "../view/SpriteFactory.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+using std::shared_ptr;
 
 class AbstractFactory {
 public:
 
     virtual ~AbstractFactory() = default;
 
-    virtual Ghost* createGhost() = 0;
-    virtual PacMan* createPacMan() = 0;
-    virtual Wall* createWall(const WallType wallType) = 0;
-    virtual Coin* createCoin() = 0;
-    virtual Fruit* createFruit() = 0;
+    virtual shared_ptr<Ghost> createGhost(GhostType type, std::tuple<double, double, double, double> position) = 0;
+    virtual shared_ptr<PacMan> createPacMan() = 0;
+    virtual shared_ptr<Wall> createWall(std::tuple<double, double, double, double> position) = 0;
+    virtual shared_ptr<Coin> createCoin() = 0;
+    virtual shared_ptr<Fruit> createFruit() = 0;
 
 };
 
