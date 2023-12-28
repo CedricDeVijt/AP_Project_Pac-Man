4ac3d867a3b0aae264bf0acd17d3727ffc9ac51d
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-28 15:28:47 +0100

    added function for walls

--- src/view/EntityView.cpp
+++ src/view/EntityView.cpp
@@ -1,1 +1,29 @@
 #include "EntityView.h"
+#include "SpriteFactory.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+//PacManView::PacManView(shared_ptr<PacMan> pacMan) {
+//
+//}
+
+WallView::WallView(std::tuple<double, double, double, double> position) : position(position) {
+}
+
+void WallView::update() {
+    std::cout << "WallView::update() Wallview was updated\n";
+    // TODO do this properly using camera
+    double x, y, size_x, size_y;
+    std::tie(x, y, size_x, size_y) = position;
+    std::cout << "Creating wall at " << x << ", " << y << "\n";
+    std::cout << "New position at " << 30 * 20 * (x + 1) << ", " << 30 * 11 * (y + 1) << "\n";
+
+//    sf::RectangleShape rectangle(sf::Vector2f(64, 64));
+    sf::RectangleShape rectangle(sf::Vector2f(60, 60));
+    // Set the position of the rectangle
+//    rectangle.setPosition(600*(x+1), 400*(y+1));
+    rectangle.setPosition(30 * 20 * (x + 1), 30 * 11 * (y + 1));
+    // Set the color of the rectangle (optional)
+    rectangle.setFillColor(sf::Color::Blue);
+    window->draw(rectangle);
+}
--- src/view/EntityView.h
+++ src/view/EntityView.h
@@ -2,28 +2,47 @@
 #define AP_PROJECT_PAC_MAN_ENTITYVIEW_H
 
 
-class EntityView {
+#include "../model/PacMan.h"
+#include "WindowSingleton.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
 
+using std::shared_ptr;
+
+class EntityView : public Observer {
+protected:
+    shared_ptr<sf::RenderWindow> window = WindowSingleton::getInstance().getWindow();
 };
 
-class PacManView : public EntityView {
-
-};
-
-class GhostView : public EntityView {
-
-};
-
-class CoinView : public EntityView {
-
-};
+//class PacManView : public EntityView {
+//public:
+//    PacManView(shared_ptr<PacMan> pacMan);
+//
+//private:
+//    shared_ptr <PacMan> pacMan;
+//};
+//
+//class GhostView : public EntityView {
+//
+//};
+//
+//class CoinView : public EntityView {
+//
+//};
 
 class WallView : public EntityView {
+public:
+    WallView(std::tuple<double, double, double, double> position);
 
+    void update() override;
+
+private:
+    // TODO pull up to EntityView
+    std::tuple<double, double, double, double> position;
 };
 
-class FruitView : public EntityView {
-
-};
+//class FruitView : public EntityView {
+//
+//};
 
 #endif //AP_PROJECT_PAC_MAN_ENTITYVIEW_H
