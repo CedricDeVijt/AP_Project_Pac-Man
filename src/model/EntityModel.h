c61dfaba1d245a7e1d15568e34aed1c26e676813
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-28 15:28:30 +0100

    added functions to class

--- src/model/EntityModel.h
+++ src/model/EntityModel.h
@@ -4,8 +4,16 @@
 #include "Subject.h"
 
 class EntityModel : public Subject {
+public:
+//    virtual void update();
+    std::tuple<double, double, double, double> getPosition();
 
+    bool overlapsWith(EntityModel entityModel);
+
+    void update();
+
+private:
+    std::tuple<double, double, double, double> position;
 };
-
 
 #endif //AP_PROJECT_PAC_MAN_ENTITYMODEL_H
