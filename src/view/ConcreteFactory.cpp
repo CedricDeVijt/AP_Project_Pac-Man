75d5c8005f817d41ca3cd11615b90949b8cfcc27
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-30 20:31:53 +0100

    view uses model directly

--- src/view/ConcreteFactory.cpp
+++ src/view/ConcreteFactory.cpp
@@ -3,7 +3,7 @@
 
 shared_ptr<Ghost> ConcreteFactory::createGhost(GhostType type, std::tuple<double, double, double, double> position) {
     shared_ptr<Ghost> ghost = std::make_shared<Ghost>(type, position);
-    shared_ptr<GhostView> ghostView = std::make_shared<GhostView>(type, position);
+    shared_ptr<GhostView> ghostView = std::make_shared<GhostView>(ghost);
     ghost->registerObserver(ghostView);
     return ghost;
 
@@ -17,22 +17,22 @@ shared_ptr<PacMan> ConcreteFactory::createPacMan(std::tuple<double, double, doub
 }
 
 shared_ptr<Wall> ConcreteFactory::createWall(std::tuple<double, double, double, double> position) {
-    shared_ptr<Wall> wall = std::make_shared<Wall>();
-    shared_ptr<WallView> wallView = std::make_shared<WallView>(position);
+    shared_ptr<Wall> wall = std::make_shared<Wall>(position);
+    shared_ptr<WallView> wallView = std::make_shared<WallView>(wall);
     wall->registerObserver(wallView);
     return wall;
 }
 
 shared_ptr<Coin> ConcreteFactory::createCoin(std::tuple<double, double, double, double> position) {
-    shared_ptr<Coin> coin = std::make_shared<Coin>();
-    shared_ptr<CoinView> coinView = std::make_shared<CoinView>(position);
+    shared_ptr<Coin> coin = std::make_shared<Coin>(position);
+    shared_ptr<CoinView> coinView = std::make_shared<CoinView>(coin);
     coin->registerObserver(coinView);
     return coin;
 }
 
 shared_ptr<Fruit> ConcreteFactory::createFruit(std::tuple<double, double, double, double> position) {
-    shared_ptr<Fruit> fruit = std::make_shared<Fruit>();
-    shared_ptr<FruitView> fruitView = std::make_shared<FruitView>(position);
+    shared_ptr<Fruit> fruit = std::make_shared<Fruit>(position);
+    shared_ptr<FruitView> fruitView = std::make_shared<FruitView>(fruit);
     fruit->registerObserver(fruitView);
     return fruit;
 }
