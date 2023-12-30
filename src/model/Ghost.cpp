2fafc4c9542a7d222335d6cecf56a33973ab3c65
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-30 20:36:44 +0100

    initial implementation of ghost behavior

--- src/model/Ghost.cpp
+++ src/model/Ghost.cpp
@@ -1,26 +1,70 @@
 #include "Ghost.h"
+#include <cmath>
+
 
 Ghost::Ghost(GhostType type, std::tuple<double, double, double, double> homePosition)
-        : type(type), homePosition(homePosition) {}
+        : type(type), homePosition(homePosition) , EntityModel(homePosition){
+    // start in waitmode
+    fearMode = false;
+    chaseMode = false;
+    waitMode = true;
+    position = homePosition;
+    // TODO set startTime Wait Mode
+
+    // set initial travel direction
+    switch (type) {
+        case GhostType::Pinky:
+        case GhostType::Inky:
+            direction = Direction::UP;
+            break;
+        case GhostType::Blinky:
+            direction = Direction::RIGHT;
+            break;
+        case GhostType::Clyde:
+            direction = Direction::LEFT;
+    }
+}
 
 
-// TODO implement going home
-//void Ghost::goHome() {
-//    switch (type) {
-//        case GhostType::Blinky :
-//            break;
-//        case GhostType::Inky :
-//            break;
-//        case GhostType::Clyde :
-//            break;
-//        case GhostType::Pinky :
-//            break;
-//        default:
-//            break;
-//    }
-//    notifyObservers();
-//}
+void Ghost::eaten() {
+    // go back to initial position and start chasing
+    fearMode = false;
+    chaseMode = true;
+    waitMode = false;
+    position = homePosition;
+}
 
 void Ghost::toFearMode() {
+    fearMode = true;
+    chaseMode = false;
+    // TODO set startTime Fear Mode
+}
 
+void Ghost::setTargetPosition(std::tuple<double, double, double, double> position) {
+    pacManPosition = position;
+}
+
+bool Ghost::isFearMode() const {
+    return fearMode;
+}
+
+bool Ghost::isChaseMode() const {
+    return chaseMode;
+}
+
+bool Ghost::isWaitMode() const {
+    return waitMode;
+}
+
+GhostType Ghost::getType() const {
+    return type;
+}
+
+
+double manhattanDistance(std::tuple<double, double, double, double> a, std::tuple<double, double, double, double> b) {
+    double a_x, a_y, b_x, b_y;
+    std::tie(a_x, a_y, std::ignore, std::ignore) = a;
+    std::tie(b_x, b_y, std::ignore, std::ignore) = b;
+
+    return std::abs(a_x - b_x) + std::abs(a_y - b_y);
 }
--- src/model/Ghost.h
+++ src/model/Ghost.h
@@ -3,7 +3,8 @@
 
 
 #include "EntityModel.h"
-
+#include "Direction.h"
+#include <tuple>
 
 enum class GhostType {
     Blinky, Pinky, Inky, Clyde, Fear
@@ -13,12 +14,26 @@ enum class GhostType {
 class Ghost : public EntityModel {
 public:
     Ghost(GhostType type, std::tuple<double, double, double, double> homePosition);
+    GhostType getType() const;
+    void eaten();
     void toFearMode();
+    void setTargetPosition(std::tuple<double, double, double, double> position);
+    bool isFearMode() const;
+    bool isChaseMode() const;
+    bool isWaitMode() const;
 
 private:
     GhostType type;
-    bool isFearMode;
+    bool fearMode;
+    bool chaseMode;
+    bool waitMode;
+    bool hasLeftStartingPoint;
     std::tuple<double, double, double, double> homePosition;
+    std::tuple<double, double, double, double> pacManPosition;
+    Direction direction;
+
+    //double manhattanDistance(std::tuple<double, double, double, double> a, std::tuple<double, double, double, double> b);
+
 };
 
 
