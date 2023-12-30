9530599ded12d3fae5a2da29c9d636981363fc6c
CedricDeVijt <cedric.devijt@gmail.com>
2023-12-30 20:31:20 +0100

    added implementation of collectables

--- src/model/Collectable.cpp
+++ src/model/Collectable.cpp
@@ -1,1 +1,7 @@
 #include "Collectable.h"
+
+Collectable::Collectable(std::tuple<double, double, double, double> position) : EntityModel(position) {}
+
+Fruit::Fruit(std::tuple<double, double, double, double> position) : Collectable(position) {}
+
+Coin::Coin(std::tuple<double, double, double, double> position) : Collectable(position) {}
--- src/model/Collectable.h
+++ src/model/Collectable.h
@@ -4,15 +4,18 @@
 #include "EntityModel.h"
 
 class Collectable : public EntityModel {
-
+public:
+    explicit Collectable(std::tuple<double, double, double, double> position);
 };
 
 class Coin : public Collectable {
-
+public:
+    explicit Coin(std::tuple<double, double, double, double> position);
 };
 
 class Fruit : public Collectable {
-
+public:
+    explicit Fruit(std::tuple<double, double, double, double> position);
 };
 
 #endif //AP_PROJECT_PAC_MAN_COLLECTABLE_H
