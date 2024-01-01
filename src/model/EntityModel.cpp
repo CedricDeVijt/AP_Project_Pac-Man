17c378bae020f2d8f8fc327aaed5c35edf5cad4a
CedricDeVijt <cedric.devijt@gmail.com>
2024-01-01 20:21:00 +0100

    nudge to grid moved up from pacman calculate step in entity direction

--- src/model/EntityModel.cpp
+++ src/model/EntityModel.cpp
@@ -1,6 +1,13 @@
-#include <utility>
-
 #include "EntityModel.h"
+
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include "Ghost.h"
+#include <cmath>
+#include <utility>
+#include "Stopwatch.h"
+
 
 std::tuple<double, double, double, double> EntityModel::getPosition() {
     return position;
@@ -20,8 +27,8 @@ bool EntityModel::overlapsWith(shared_ptr<EntityModel> entityModel, double perce
     std::tie(x2, y2, sizeX2, sizeY2) = entityModel->position;
 
     // Check for overlap along both axes
-    bool overlapX = (x1 < (x2 + sizeX2 * (1 - percentage))) && ((x1 + sizeX1 * (1 - percentage)) > x2);
-    bool overlapY = (y1 < (y2 + sizeY2 * (1 - percentage))) && ((y1 + sizeY1 * (1 - percentage)) > y2);
+    bool overlapX = (x1 < (x2 + sizeX2 * (1-percentage))) && ((x1 + sizeX1 * (1-percentage)) > x2);
+    bool overlapY = (y1 < (y2 + sizeY2 * (1-percentage))) && ((y1 + sizeY1 * (1-percentage)) > y2);
 
     // The rectangles overlap if they intersect along both axes
     return overlapX && overlapY;
@@ -32,4 +39,46 @@ void EntityModel::update() {
 }
 
 
+double roundToClosestMultiple(double value, double multiple) {
+    // Calculate the rounded result to the nearest integer
+    double roundedValue = std::round(value / multiple);
+
+    // Multiply by the multiple to get the closest multiple
+    double result = roundedValue * multiple;
+
+    return result;
+}
+
+void EntityModel::nudgeToGrid() {
+    double x, y, sizeX, sizeY;
+    std::tie(x, y, sizeX, sizeY) = position;
+    x = roundToClosestMultiple(x + 1, sizeX) -1;
+    y = roundToClosestMultiple(y + 1 , sizeY) -1;
+    position = std::make_tuple(x, y, sizeX, sizeY);
+}
+
+
 EntityModel::EntityModel(std::tuple<double, double, double, double> position) : position(std::move(position)) {}
+
+// calculate the position after taking a step in a direction
+std::tuple<double, double, double, double>
+EntityModel::step(Direction direction, std::tuple<double, double, double, double> &startPosition) {
+    // calculate step in each direction
+    auto deltaTime = Stopwatch::getInstance().getDeltaTime();
+    std::cout << "deltaTime = " << deltaTime << "\n";
+    double stepX = SPEED * deltaTime;
+    double stepY = stepX *22/11; // compensate for grid aspect ratio
+
+    double x, y, sizeX, sizeY;
+    std::tie(x, y, sizeX, sizeY) = startPosition;
+    if (direction == LEFT) {
+        x -= stepX;
+    } else if (direction == RIGHT) {
+        x += stepX;
+    } else if (direction == UP) {
+        y -= stepY;
+    } else if (direction == DOWN) {
+        y += stepY;
+    }
+    return std::make_tuple(x, y, sizeX, sizeY);
+}
--- src/model/EntityModel.h
+++ src/model/EntityModel.h
@@ -2,7 +2,10 @@
 #define AP_PROJECT_PAC_MAN_ENTITYMODEL_H
 
 #include "Subject.h"
+#include "Direction.h"
 #include <tuple>
+
+static const double SPEED = 2.0 / 4000000; // full screen = [-1 1] in 4 seconds (expressed as microseconds)
 
 class EntityModel : public Subject {
 public:
@@ -14,6 +17,10 @@ public:
 
 protected:
     std::tuple<double, double, double, double> position;
+    void nudgeToGrid();
+
+//double manhattanDistance(std::tuple<double, double, double, double> a, std::tuple<double, double, double, double> b);
+std::tuple<double, double, double, double> step(Direction direction, std::tuple<double, double, double, double> &startPosition);
 };
 
 #endif //AP_PROJECT_PAC_MAN_ENTITYMODEL_H
