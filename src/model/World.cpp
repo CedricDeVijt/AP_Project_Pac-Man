b85684da0c651babfd9614d44346b212e653b529
CedricDeVijt <cedric.devijt@gmail.com>
2024-01-01 17:58:09 +0100

    clean up

--- src/model/World.cpp
+++ src/model/World.cpp
@@ -1,10 +1,10 @@
 #include "World.h"
 #include "Stopwatch.h"
-#include <vector>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
-World::World(shared_ptr<AbstractFactory> factory, int level, shared_ptr<Score> score) : score(score) {
+World::World(shared_ptr<AbstractFactory> factory, int level, shared_ptr<Score> score) : score(score), level(level) {
 
     std::vector<std::string> level_1{
             "wwwwwwwwwwwwwwwwwwww",
@@ -37,9 +37,6 @@ World::World(shared_ptr<AbstractFactory> factory, int level, shared_ptr<Score> s
     int items_x = board[0].length();
     int items_y = board.size();
 
-//    std::cout << "items x: " << items_x << "\n";
-//    std::cout << "items y: " << items_y << "\n";
-
     double size_x = 2.0 / items_x;
     double size_y = 2.0 / items_y;
 
@@ -49,32 +46,32 @@ World::World(shared_ptr<AbstractFactory> factory, int level, shared_ptr<Score> s
             double position_y = i * size_y - 1.0;
             std::tuple<double, double, double, double> position(position_x, position_y, size_x, size_y);
             switch (board[i][j]) {
-                case 'w':
-                    walls.push_back(factory->createWall(position));
-                    break;
-                case 'c':
-                    coins.push_back(factory->createCoin(position));
-                    break;
-                case 'f':
-                    fruits.push_back(factory->createFruit(position));
-                    break;
-                case '1':
-                    ghosts.push_back(factory->createGhost(GhostType::Blinky, position));
-                    break;
-                case '2':
-                    ghosts.push_back(factory->createGhost(GhostType::Pinky, position));
-                    break;
-                case '3':
-                    ghosts.push_back(factory->createGhost(GhostType::Inky, position));
-                    break;
-                case '4':
-                    ghosts.push_back(factory->createGhost(GhostType::Clyde, position));
-                    break;
-                case 'P':
-                    pacMan = factory->createPacMan(position);
-                    break;
-                default:
-                    break;
+            case 'w':
+                walls.push_back(factory->createWall(position));
+                break;
+            case 'c':
+                coins.push_back(factory->createCoin(position));
+                break;
+            case 'f':
+                fruits.push_back(factory->createFruit(position));
+                break;
+            case '1':
+                ghosts.push_back(factory->createGhost(GhostType::Blinky, position));
+                break;
+            case '2':
+                ghosts.push_back(factory->createGhost(GhostType::Pinky, position));
+                break;
+            case '3':
+                ghosts.push_back(factory->createGhost(GhostType::Inky, position));
+                break;
+            case '4':
+                ghosts.push_back(factory->createGhost(GhostType::Clyde, position));
+                break;
+            case 'P':
+                pacMan = factory->createPacMan(position);
+                break;
+            default:
+                break;
             }
         }
     }
@@ -82,7 +79,7 @@ World::World(shared_ptr<AbstractFactory> factory, int level, shared_ptr<Score> s
 
 void World::update() {
     Stopwatch::getInstance().tick();
-    for (auto &ghost: ghosts) {
+    for (auto& ghost : ghosts) {
         ghost->update();
     }
 
@@ -91,19 +88,17 @@ void World::update() {
     collect(coins);
     collect(fruits);
 
-    for (auto &coin: coins) {
+    for (auto& coin : coins) {
         coin->update();
     }
 
-    for (auto &fruit: fruits) {
+    for (auto& fruit : fruits) {
         fruit->update();
     }
 
-    for (auto &wall: walls) {
+    for (auto& wall : walls) {
         wall->update();
     }
-
-
 }
 
 void World::collect(std::vector<std::shared_ptr<Coin>>& collectables) {
@@ -121,7 +116,6 @@ void World::collect(std::vector<std::shared_ptr<Coin>>& collectables) {
     collectables.erase(newEnd, collectables.end());
 }
 
-
 void World::collect(std::vector<std::shared_ptr<Fruit>>& collectables) {
     // Define the condition for removal (overlap with PacMan)
     auto condition = [this](const std::shared_ptr<Collectable>& collectable) {
@@ -131,7 +125,7 @@ void World::collect(std::vector<std::shared_ptr<Fruit>>& collectables) {
     auto newEnd = std::remove_if(collectables.begin(), collectables.end(), condition);
     // before removing, do something
     for (auto it = newEnd; it != collectables.end(); ++it) {
-        for (auto ghost: ghosts) {
+        for (auto ghost : ghosts) {
             ghost->toFearMode();
             score->pacManCapturesGhost();
         }
@@ -140,9 +134,7 @@ void World::collect(std::vector<std::shared_ptr<Fruit>>& collectables) {
     collectables.erase(newEnd, collectables.end());
 }
 
-void World::setDirectionPacMan(const Direction &direction) {
-   pacMan->setTargetDirection(direction);
-}
+void World::setDirectionPacMan(const Direction& direction) { pacMan->setTargetDirection(direction); }
 
 std::vector<Direction> World::getPossibleDirections(double tolerance) {
     // Get PacMan's current position and size
@@ -156,11 +148,10 @@ std::vector<Direction> World::getPossibleDirections(double tolerance) {
     std::vector<Direction> possibleDirections = {LEFT, RIGHT, UP, DOWN};
 
     // Check if PacMan collides with the wall
-    for (auto &wall : walls) {
+    for (auto& wall : walls) {
         // Get wall's position and size
         double wallX, wallY, wallSizeX, wallSizeY;
         std::tie(wallX, wallY, wallSizeX, wallSizeY) = wall->getPosition();
-
 
         // Check UP DOWN, reduce width of box
         // Check for collision along the X-axis
@@ -169,28 +160,38 @@ std::vector<Direction> World::getPossibleDirections(double tolerance) {
         // If there is a collision along both axes, PacMan cannot move in that direction
         if (collisionX && collisionY) {
             if (y < wallY) {
-                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), DOWN), possibleDirections.end());
+                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), DOWN),
+                                         possibleDirections.end());
             } else if (y + sizeY > wallY + wallSizeY) {
-                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), UP), possibleDirections.end());
+                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), UP),
+                                         possibleDirections.end());
             }
         }
 
         // Check LEFT & RIGHT, reduce Height of box
         // Check for collision along the X-axis and Y-axis
-        collisionX = x - toleranceX < wallX + wallSizeX && x + sizeX + toleranceX> wallX;
+        collisionX = x - toleranceX < wallX + wallSizeX && x + sizeX + toleranceX > wallX;
         collisionY = y + toleranceY < wallY + wallSizeY && y + sizeY - toleranceY > wallY;
         // If there is a collision along both axes, PacMan cannot move in that direction
         if (collisionX && collisionY) {
             // Remove the corresponding direction from the possible directions
             if (x < wallX) {
-                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), RIGHT), possibleDirections.end());
+                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), RIGHT),
+                                         possibleDirections.end());
             } else if (x + sizeX > wallX + wallSizeX) {
-                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), LEFT), possibleDirections.end());
+                possibleDirections.erase(std::remove(possibleDirections.begin(), possibleDirections.end(), LEFT),
+                                         possibleDirections.end());
             }
         }
-
     }
 
     // Return the updated list of possible directions after checking for collisions with walls
     return possibleDirections;
 }
+bool World::isLevelComplete() {
+    return coins.size() == 30;
+
+    // TODO put back
+    //    return coins.empty();
+}
+bool World::isAllLevelsComplete() { return isLevelComplete() && level == 2; }
