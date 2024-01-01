cef3fd78f12864a2fe021703c158cc6542be7342
CedricDeVijt <cedric.devijt@gmail.com>
2024-01-01 20:19:59 +0100

    print values for direction class

--- src/model/Direction.cpp
+++ src/model/Direction.cpp
@@ -1,5 +1,41 @@
-//
-// Created by Cedric De Vijt on 30/12/2023.
-//
-
 #include "Direction.h"
+
+std::ostream& operator<<(std::ostream& os, const Direction& dir) {
+    switch (dir) {
+        case LEFT:
+            os << "LEFT";
+            break;
+        case RIGHT:
+            os << "RIGHT";
+            break;
+        case UP:
+            os << "UP";
+            break;
+        case DOWN:
+            os << "DOWN";
+            break;
+        case NONE:
+            os << "NONE";
+            break;
+        default:
+            os << "UNKNOWN";
+            break;
+    }
+    return os;
+}
+
+//Direction oppositeDirection(Direction dir) {
+//    switch (dir) {
+//        case LEFT:
+//            return RIGHT;
+//        case RIGHT:
+//            return LEFT;
+//        case UP:
+//            return DOWN;
+//        case DOWN:
+//            return UP;
+//        case NONE:
+//            return NONE;
+//    }
+//}
+//
--- src/model/Direction.h
+++ src/model/Direction.h
@@ -1,10 +1,13 @@
 #ifndef AP_PROJECT_PAC_MAN_DIRECTION_H
 #define AP_PROJECT_PAC_MAN_DIRECTION_H
+#include <iostream>
 
 
 enum Direction {
     LEFT, RIGHT, UP, DOWN, NONE
 };
 
+Direction oppositeDirection(Direction dir);
+std::ostream& operator<<(std::ostream& os, const Direction& dir);
 
 #endif //AP_PROJECT_PAC_MAN_DIRECTION_H
