b2d1647db64c341cf3b67d13b3a6789ae1bf521b
CedricDeVijt <cedric.devijt@gmail.com>
2024-01-03 20:22:33 +0100

    singleton template generation

---
+++ src/util/Singleton.h
@@ -0,0 +1,23 @@
+#ifndef GAMELOGICLIB_SINGLETON_H
+#define GAMELOGICLIB_SINGLETON_H
+
+#include <iostream>
+
+template <typename T>
+class Singleton {
+public:
+    // get the static instance
+    static T& getInstance() {
+            static T instance;  // The instance is created only once
+            return instance;
+    };
+
+private:
+    Singleton() {}  // Private constructor to prevent external instantiation
+    ~Singleton() {} // Optional: Destructor to ensure proper cleanup
+    Singleton(const Singleton&) = delete;             // Disable copy constructor
+    Singleton& operator=(const Singleton&) = delete;  // Disable copy assignment
+};
+
+
+#endif //GAMELOGICLIB_SINGLETON_H
